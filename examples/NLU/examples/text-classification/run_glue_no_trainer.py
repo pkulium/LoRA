47c89c9c31d032f5166cf46afdc684bb20ed1110
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-02 14:51:51 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   examples/NLU/examples/text-classification/run_glue_no_trainer.py

--- examples/NLU/examples/text-classification/run_glue_no_trainer.py
+++ examples/NLU/examples/text-classification/run_glue_no_trainer.py
@@ -559,8 +559,8 @@ def main():
     optimizer, weight_opt = get_optimizer(args, model)
 
     # Prepare everything with our `accelerator`.
-    model, weight_opt, train_dataloader, eval_dataloader = accelerator.prepare(
-        model, weight_opt, train_dataloader, eval_dataloader
+    model, weight_opt, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(
+        model, weight_opt, optimizer, train_dataloader, eval_dataloader
     )
 
     # Note -> the training dataloader needs to be prepared before we grab h`is length below (cause its length will be
