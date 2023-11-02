b1bc12753cc9536214d01e319e0f1769df8eff53
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-02 10:40:09 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   example_loralib.py
    	renamed:    lora_custom/__init__.py -> loralib/__init__.py
    	renamed:    lora_custom/layers.py -> loralib/layers.py
    	renamed:    lora_custom/utils.py -> loralib/utils.py

--- example_loralib.py
+++ example_loralib.py
@@ -15,8 +15,8 @@ import torch
 import torch.nn as nn
 from torch.optim import Adam
 from torchsummary import summary
-import loralib as lora
-import lora_custom
+import loralib as loralib
+import loralib
 from transformers import DataCollatorForSeq2Seq, get_cosine_schedule_with_warmup
 from torch.utils.data import Dataset, DataLoader
 from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
@@ -31,7 +31,7 @@ from transformers import CONFIG_MAPPING
 from transformers import AutoConfig
 
 def make_lora_layer(layer, lora_r=16):
-    new_layer = lora_custom.Linear(
+    new_layer = loralib.Linear(
         in_features=layer.in_features,
         out_features=layer.out_features,
         bias=layer.bias is None,
@@ -121,7 +121,7 @@ total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires
 print(f"Total trainable parameters before LoRA: {total_trainable_params}")
 
 ## Apply LoRA
-lora_custom.mark_only_lora_as_trainable(model)
+loralib.mark_only_lora_as_trainable(model)
 
 total_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print(f"Total trainable parameters after LoRA: {total_trainable_params}")
@@ -229,7 +229,7 @@ for i in range(epochs):
         checkpoint_path = "best_lora_checkpoint.pth"
         
         if USE_LORA:
-            torch.save(lora_custom.lora_state_dict(model), checkpoint_path)
+            torch.save(loralib.lora_state_dict(model), checkpoint_path)
         else:
             torch.save(model.state_dict(), checkpoint_path)
 
